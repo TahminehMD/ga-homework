543db4611b326b31598b3742a52a9c209887fad7
tahmineh darvish <tahmineh5314@gmail.com>
2022-10-13 12:24:26 -0700

    have done project 4

--- Homework 04/script.js
+++ Homework 04/script.js
@@ -1,11 +1,41 @@
 let score = 0
 
-let ball = document.querySelector("img")
-ball.onclick = function(){
-    score++
-    let scoreH1 = document.querySelector("h1")
-    scoreH1.innerHTML = "Your score is : " + score
-    if(score == 10){
-        window.alert("you win!!")
-    }
+let ball1 = document.getElementById("ball1")
+let ball2 = document.getElementById("ball2")
+let ball3 = document.getElementById("ball3")
+let scoreText = document.getElementById("score-text")
+
+ball1.onclick = function () {
+    score = score + 10
+    scoreText.innerHTML = score
 }
+
+ball2.onclick = function () {
+    score = score + 8
+    scoreText.innerHTML = score
+}
+
+ball3.onclick = function () {
+    score = score + 15
+    scoreText.innerHTML = score
+}
+
+let balls = document.querySelectorAll(".ball")
+for (let i = 0; i < 3; i++) {
+    balls[i].addEventListener('mouseover', function () {
+        balls[i].style = "border : 6px solid black"
+    })
+    balls[i].addEventListener('mouseleave', function () {
+        balls[i].style = "border : none"
+    })
+}
+
+setInterval(function () {
+    if (score >= 100) {
+        window.alert("YOU WIN!!")
+        score = 0
+        scoreText.innerHTML = score
+    }
+}, 500)
+
+
