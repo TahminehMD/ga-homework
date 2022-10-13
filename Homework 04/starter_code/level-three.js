392e826694ec2afb63a3b6a38dbb3daa97410a48
tahmineh darvish <tahmineh5314@gmail.com>
2022-10-13 12:25:38 -0700

    delete unnessary file

--- Homework 04/starter_code/level-three.js
+++
@@ -1,7 +0,0 @@
-/*
- * DOTS: Level Three
- *
- */
-let score = 0;
-let arenaEl = document.querySelector('.js-arena');
-let scoreEl = document.querySelector('.js-score');
--- Homework 04/starter_code/level-two.js
+++
@@ -1,9 +0,0 @@
-/*
- * DOTS: Level Two
- *
- */
-let score = 0;
-let arenaEl = document.querySelector('.js-arena');
-let scoreEl = document.querySelector('.js-score');
-
-
