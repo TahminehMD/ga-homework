be7aa0c16ee7ff61514050beb196d9c3a2a11b3e
tahmineh darvish <tahmineh5314@gmail.com>
2022-10-16 11:45:05 -0700

    have done 2part

--- Homework 04 redocopy/starter_code/level-two.js
+++ Homework 04 redocopy/starter_code/level-two.js
@@ -6,4 +6,22 @@ let score = 0;
 let arenaEl = document.querySelector('.js-arena');
 let scoreEl = document.querySelector('.js-score');
 
+let balls = document.querySelectorAll("js-ball")
+for (let i = 0; i < 3; i++) {
+    balls[i].addEventListener('click', function () {
+        score = score + 10
+
+        if (score <= 100) {
+            scoreEl.innerText = score;
+            //document.getElementById("test").innerHTML= score   (second way)
+        } else {
+            declareWinner();
+        }
+    })
+}
+function declareWinner() {
+    document.body.classList.add('game-over');
+}
+
+
 
